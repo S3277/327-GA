3b88937f1d0f0b836496f2803c40bb9ddd4fe32c
souhayl farsane <souhayl.farsane.1@ens.etsmtl.ca>
2026-03-20 00:43:47 -0400

    Remove '327' text from Onboarding logo

--- src/components/Onboarding.tsx
+++ src/components/Onboarding.tsx
@@ -54,16 +54,13 @@ export function Onboarding({ onComplete }: OnboardingProps) {
           initial={{ y: 20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ delay: 0.2, duration: 0.8 }}
-          className="mb-6 sm:mb-8 flex flex-col items-center"
+          className="mb-6 sm:mb-8"
         >
           <img
             src='/IMG_3904.png'
             alt='327 Logo'
-            className='h-20 w-20 sm:h-24 sm:w-24 md:h-32 md:w-32 rounded object-cover mb-4'
+            className='h-24 w-24 sm:h-32 sm:w-32 md:h-40 md:w-40 rounded object-cover'
           />
-          <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-white tracking-tighter">
-            327
-          </div>
         </motion.div>
 
         <motion.div
